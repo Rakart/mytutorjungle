fc3f86a8de453477c32ee6286ea5e644188a17dc
Rakart <carlos.greblo@gmail.com>
2015-06-17 00:58:43 +0800

    added modals to edit tutors

--- public/modules/tutors/controllers/tutors.client.controller.js
+++ public/modules/tutors/controllers/tutors.client.controller.js
@@ -1,10 +1,54 @@
 'use strict';
 
-// Tutors controller
-angular.module('tutors').controller('TutorsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Tutors',
-	function($scope, $stateParams, $location, Authentication, Tutors) {
-		$scope.authentication = Authentication;
+var tutorsApp = angular.module('tutors');
 
+// Tutors controller
+tutorsApp.controller('TutorsController', ['$scope', '$stateParams', 'Authentication', 'Tutors', '$modal', '$log',
+	function($scope, $stateParams, Authentication, Tutors, $modal, $log) {
+		this.authentication = Authentication;
+
+		// Find a list of Tutors
+		this.tutors = Tutors.query();
+
+		// Open a modal window to update a single registered tutor
+		this.modalUpdate = function (size, selectedTutor) {
+
+		    var modalInstance = $modal.open({
+		      templateUrl: 'modules/tutors/views/edit-tutor.client.view.html',
+		      controller: function($scope, $modalInstance, tutor) {
+		      	$scope.tutor = tutor;
+		      },
+		      size: size,
+		      resolve: {
+		        tutor: function () {
+		          return selectedTutor;
+		        }
+		      }
+		    });
+
+		    modalInstance.result.then(function (selectedItem) {
+		      	$scope.selected = selectedItem;
+		    }, function () {
+	      		$log.info('Modal dismissed at: ' + new Date());
+		    });
+	  	};
+	}
+]);
+
+// Tutors create controller
+tutorsApp.controller('TutorsCreateController', ['$scope', 'Tutors',
+	function($scope, Tutors) {
+	}
+]);
+
+// Tutors edit controller
+tutorsApp.controller('TutorsUpdateController', ['$scope', 'Tutors',
+	function($scope, Tutors) {
+	}
+]);
+
+		
+/*
 		// Create new Tutor
 		$scope.create = function() {
 			// Create new Tutor object
@@ -64,10 +108,7 @@ angular.module('tutors').controller('TutorsController', ['$scope', '$stateParams
 			});
 		};
 
-		// Find a list of Tutors
-		$scope.find = function() {
-			$scope.tutors = Tutors.query();
-		};
+
 
 		// Find existing Tutor
 		$scope.findOne = function() {
@@ -75,5 +116,5 @@ angular.module('tutors').controller('TutorsController', ['$scope', '$stateParams
 				tutorId: $stateParams.tutorId
 			});
 		};
-	}
-]);
+
+		*/
